fdb43c7c8f42145f89a55fbb2c3b861b8bc72bb5
Mariano Giagante <mariano.giagante@gmail.com>
2017-08-02 06:02:39 -0300

    Includes rack-test in the rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # Makes rack-test methods and attributes available in rspec examples.
+  config.include Rack::Test::Methods
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
