b5b722d2bb84c815f499e1079032eac15c84f5e3
Mariano Giagante <mariano.giagante@gmail.com>
2017-08-03 00:50:45 -0300

    Implements 2 more tests

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -2,10 +2,15 @@ ENV['RACK_ENV'] = 'test'
 
 #require 'app'  # <-- your sinatra app
 #require 'rspec'
+require 'pry'
 
 describe 'The Hangman App' do
   def app
     Sinatra::Application
+  end
+
+  before :all do
+    clear_cookies
   end
 
   it "shows the game page" do
@@ -17,13 +22,37 @@ describe 'The Hangman App' do
   it "provides a field to guess on letters" do
     get '/'
     expect(last_response).to be_ok
-    # Test that there's a text field
+    expect(last_response.body).to include('<form')
+    expect(last_response.body).to include('<input type="text"')
   end
 
   it "consumes 1 remaining try each time the player guesses wrong" do
+    valid_letters = ('a'..'z').to_a
+
+    secret_word = last_request.session[:secret_word]
+    right_guesses = secret_word.split('')
+    wrong_guesses = valid_letters - right_guesses
+
+    remaining_tries_before_guess = last_request.session[:guesses_left]
+    3.times do
+      post "/", { guess: wrong_guesses.sample } # Any of the wrong letters will do, but randomizing it is nice.
+    end
+    remaining_tries_after_guess = last_request.session[:guesses_left]
+
+    expect(remaining_tries_after_guess).to eql(remaining_tries_before_guess - 3)
   end
 
   it "does not consume any remaining tries each time the player guesses right" do
+    secret_word = last_request.session[:secret_word]
+    right_guesses = secret_word.split('')
+
+    remaining_tries_before_guess = last_request.session[:guesses_left]
+    3.times do
+      post "/", { guess: right_guesses.sample } # Any of the right letters will do, but randomizing it is nice.
+    end
+    remaining_tries_after_guess = last_request.session[:guesses_left]
+
+    expect(remaining_tries_after_guess).to eql(remaining_tries_before_guess)
   end
 
   it "marks letters guessed right in all positions they appear in the word" do
