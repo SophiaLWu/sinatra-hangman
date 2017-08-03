82b6f37c2743a41f138d01bfa2d344fe71617b1e
Mariano Giagante <mariano.giagante@gmail.com>
2017-08-03 01:57:10 -0300

    * Replaces :all for :each in before block to actually make 1 get to / before each example.
    Implements test for winning

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -4,12 +4,13 @@ ENV['RACK_ENV'] = 'test'
 #require 'rspec'
 require 'pry'
 
+# TODO: Refactor: Extract Method on the logic to get letters for right / wrong guesses given a secret word.
 describe 'The Hangman App' do
   def app
     Sinatra::Application
   end
 
-  before :all do
+  before :each do
     get '/'
   end
 
@@ -77,5 +78,15 @@ describe 'The Hangman App' do
   end
 
   it "shows the player won the game if they guess the whole word before running out of tries" do
+    secret_word = last_request.session[:secret_word]
+    right_guesses = secret_word.split('')
+
+    secret_word.size.times do
+      selected_guess = right_guesses.delete(right_guesses.sample) # Deletes a random letter from right_guesses and stores it into selected_guess
+      post "/", { guess: selected_guess }
+    end
+    expect(last_response.body).to include("You win!")
+
+    # TODO: Ask why expect(last_response.body).to include("You lose!") is not failing this test.
   end
 end
