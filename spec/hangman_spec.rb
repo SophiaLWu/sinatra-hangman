1795764743f8ea4d6a5370d4c168bb83c93a4f6d
Mariano Giagante <mariano.giagante@gmail.com>
2017-08-05 17:06:53 -0300

    Refactors the logic to obtain right and wrong guesses on a word; Removes comments

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -1,13 +1,20 @@
 ENV['RACK_ENV'] = 'test'
 
-#require 'app'  # <-- your sinatra app
-#require 'rspec'
-require 'pry'
-
-# TODO: Refactor: Extract Method on the logic to get letters for right / wrong guesses given a secret word.
 describe 'The Hangman App' do
   def app
     Sinatra::Application
+  end
+
+  def right_guesses_on(secret_word, number_of_guesses)
+    right_guesses = secret_word.split('')
+    right_guesses.sample(number_of_guesses)
+  end
+
+  def wrong_guesses_on(secret_word, number_of_guesses)
+    valid_letters = ('a'..'z').to_a
+    right_guesses = secret_word.split('')
+    wrong_guesses = valid_letters - right_guesses
+    wrong_guesses.sample(number_of_guesses)
   end
 
   before :each do
@@ -26,16 +33,11 @@ describe 'The Hangman App' do
   end
 
   it "consumes 1 remaining try each time the player guesses wrong" do
-    valid_letters = ('a'..'z').to_a
-
     secret_word = last_request.session[:secret_word]
-    right_guesses = secret_word.split('')
-    wrong_guesses = valid_letters - right_guesses
 
     remaining_tries_before_guess = last_request.session[:guesses_left]
-    3.times do
-      selected_guess = wrong_guesses.delete(wrong_guesses.sample) # Deletes a random letter from wrong_guesses and stores it into selected_guess
-      post "/", { guess: selected_guess }
+    wrong_guesses_on(secret_word, 3).each do |wrong_guess|
+      post '/', { guess: wrong_guess }
     end
     remaining_tries_after_guess = last_request.session[:guesses_left]
 
@@ -44,31 +46,21 @@ describe 'The Hangman App' do
 
   it "does not consume any remaining tries each time the player guesses right" do
     secret_word = last_request.session[:secret_word]
-    right_guesses = secret_word.split('')
 
     remaining_tries_before_guess = last_request.session[:guesses_left]
-    3.times do
-      selected_guess = right_guesses.delete(right_guesses.sample) # Deletes a random letter from right_guesses and stores it into selected_guess
-      post "/", { guess: selected_guess }
+    right_guesses_on(secret_word, 3).each do |right_guess|
+      post "/", { guess: right_guess }
     end
     remaining_tries_after_guess = last_request.session[:guesses_left]
 
     expect(remaining_tries_after_guess).to eql(remaining_tries_before_guess)
   end
 
-  it "does not mark any letter if the pĺayer guesses wrong" do
-  end
-
   it "shows the player lost the game if they run out of tries before guessing the whole word" do
-    valid_letters = ('a'..'z').to_a
-
     secret_word = last_request.session[:secret_word]
-    right_guesses = secret_word.split('')
-    wrong_guesses = valid_letters - right_guesses
 
-    10.times do
-      selected_guess = wrong_guesses.delete(wrong_guesses.sample) # Deletes a random letter from wrong_guesses and stores it into selected_guess
-      post "/", { guess: selected_guess }
+    wrong_guesses_on(secret_word, 10).each do |wrong_guess|
+      post '/', { guess: wrong_guess }
     end
 
     expect(last_response.body).to include("You lose!")
@@ -76,11 +68,9 @@ describe 'The Hangman App' do
 
   it "shows the player won the game if they guess the whole word before running out of tries" do
     secret_word = last_request.session[:secret_word]
-    right_guesses = secret_word.split('')
 
-    secret_word.size.times do
-      selected_guess = right_guesses.delete(right_guesses.sample) # Deletes a random letter from right_guesses and stores it into selected_guess
-      post "/", { guess: selected_guess }
+    right_guesses_on(secret_word, secret_word.size).each do |right_guess|
+      post '/', { guess: right_guess }
     end
     expect(last_response.body).to include("You win!")
 
